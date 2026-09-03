783647a7f4bc6e395363b1cc5da3d88bbb29a7f4
srihari03-sh <srihariboora03@gmail.com>
2026-09-03 19:43:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "filename" {
+  description = "File path where the pet file will be created"
+  type        = string
+  default     = "/tmp/pets.txt"
+}
+variable "content" {
+  description = "Content to be written into the pet file"
+  type        = string
+  default     = "my horse is black in colour!"
+}
+variable "prefix" {
+  description = "Prefix for the random pet name"
+  type        = string
+  default     = "MB"
+}
+variable "separator" {
+  description = "Separator used in the random pet name"
+  type        = string
+  default     = "."
+}
+variable "length" {
+  description = "Number of words in the random pet name"
+  type        = number
+  default     = 1
+}
