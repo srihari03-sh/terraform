52d077d3d3c013d6b7e7283a6ef68f2011172ca7
srihari03-sh <srihariboora03@gmail.com>
2026-09-07 20:50:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,24 @@
-variable "filename" {
-  description = "File path where the pet file will be created"
-  type        = string
-  default     = "/tmp/pets.txt"
+variable "pet_filename" {
+  type    = string
+  default = "pets.txt"
 }
-variable "content" {
-  description = "Content to be written into the pet file"
-  type        = string
-  default     = "my horse is black in colour!"
+
+variable "pet_content" {
+  type    = string
+  default = "my horse is black in colour!"
 }
-variable "prefix" {
-  description = "Prefix for the random pet name"
-  type        = string
-  default     = "MR"
+
+variable "random_pet_prefix" {
+  type    = string
+  default = "MR"
 }
-variable "separator" {
-  description = "Separator used in the random pet name"
-  type        = string
-  default     = "."
+
+variable "random_pet_separator" {
+  type    = string
+  default = "."
 }
-variable "length" {
-  description = "Number of words in the random pet name"
-  type        = number
-  default     = 1
+
+variable "random_pet_length" {
+  type    = number
+  default = 1
 }
